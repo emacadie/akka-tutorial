939e00cda59f50f98b1c3b2670f9c9877ac56857
Eric MacAdie <Eric@MacAdie.net>
2013-07-08 23:49:05 -0500

    trying to test

--- src/test/java/info/shelfunit/testing/MyActorTest.java
+++ src/test/java/info/shelfunit/testing/MyActorTest.java
@@ -2,8 +2,6 @@ package info.shelfunit.testing;
 
 import info.shelfunit.testing.MyActor;
 
-import org.apache.james.admin.webapp.beans.UserManagerBean;
-import javax.faces.event.ActionEvent;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,12 +9,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.apache.james.admin.webapp.hibernate.NewHibernateUtil;
-import org.apache.james.admin.webapp.hibernate.pojos.Users;
-import org.hibernate.Query;
-import org.hibernate.Session;
 import java.util.List;
 import java.util.UUID;
+
+import akka.actor.Props;
+import akka.testkit.TestActorRef;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 
 /**
  *
@@ -27,200 +27,43 @@ public class MyActorTest {
     private String username;
     private String hibernateUserName;
 
-    public UserManagerBeanTest() {
+    public MyActorTest() {
     }
 
     @BeforeClass
-	public static void setUpClass() throws Exception {
+    public static void setUpClass() throws Exception {
 
     }
 
     @AfterClass
-	public static void tearDownClass() throws Exception {
+    public static void tearDownClass() throws Exception {
     }
 
     @Before
-	public void setUp() {
+    public void setUp() {
         UUID uuid = UUID.randomUUID();
-        this.setUsername( uuid.toString() );
+        // this.setUsername( uuid.toString() );
         UUID hibernateUuid = UUID.randomUUID();
-        this.setHibernateUserName( hibernateUuid.toString() );
+        // this.setHibernateUserName( hibernateUuid.toString() );
     } // end method setUp
 
     @After
-	public void tearDown() {
+    public void tearDown() {
     }
 
-
-    /**
-     * Test of digestString method, of class UserManagerBean.
-     */
     @Test
-	public void testDigestString() throws Exception {
-        System.out.println( "digestString" );
-
-        Integer theInt = new Integer( this.hashCode() );
-        String pass = theInt.toString();
-        String algorithm = "SHA-256";
-        UserManagerBean instance = new UserManagerBean();
-        String result1 = instance.digestString( pass, algorithm );
-        String result2 = instance.digestString( pass, algorithm );
-        System.out.println( "Here is the password: " + pass );
-        System.out.println( "result1: " + result1 );
-        System.out.println( "result2: " + result2 );
-        org.junit.Assert.assertEquals( result1, result2 );
-    } // end method testDigestString()
-
-    /**
-     * Test of addUser method, of class UserManagerBean. Testing the underlying
-     * Hibernate API
-     */
-    @Test
-	public void testAddUserHibernate() throws Exception {
-        
-        System.out.println( "testAddUserHibernate" );
-        UserManagerBean instance = new UserManagerBean();
-
-        Session session = NewHibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-
-        Integer theInt = new Integer( this.hashCode() );
-        String pass = theInt.toString();
-        String algorithm = "SHA-256";
-        String queryString = "from Users where username like :theName"
-	    + " order by username";
-
-        Query q = session.createQuery( queryString );
-        q.setString( "theName", this.getHibernateUserName() + "%" );
-        List userList = q.list();
-        // user does not exist
-        org.junit.Assert.assertEquals( 0, userList.size() );
-
-        String passAlgo = instance.digestString( pass, algorithm );
-
-        Users user = new Users();
-        user.setUsername( this.getHibernateUserName() );
-        user.setPwdAlgorithm( algorithm );
-        user.setPwdHash( passAlgo );
-        session.save( user );
-
-        session.getTransaction().commit();
-
-        q = session.createQuery( queryString );
-        q.setString( "theName", this.getHibernateUserName() + "%" );
-        List userList2 = q.list();
-        session.close();
-        // user does exist
-        org.junit.Assert.assertEquals( 1, userList2.size() );
-        
-    } // end method testAddUserHibernate
-
-    /**
-     * Test the UserManagerBean.addUser method.
-     * @throws Exception
-     */
-    @Test
-	public void testAddUser() throws Exception {
-        
-        System.out.println( "testAddUser" );
-        UserManagerBean instance = new UserManagerBean();
-
-        Session session = NewHibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-
-        Integer theInt = new Integer( this.hashCode() );
-        String pass = theInt.toString();
-        String algorithm = "SHA-256";
-        String queryString = "from Users where username like :theName"
-	    + " order by username";
-
-        Query q = session.createQuery( queryString );
-        q.setString( "theName", this.getUsername() + "%" );
-        List userList = q.list();
-        // user does not exist
-        org.junit.Assert.assertEquals( 0, userList.size() );
-
-        String passAlgo = instance.digestString( pass, algorithm );
-
-        instance.setUsername( this.getUsername() );
-        instance.setPassword( pass );
-        instance.setAlgorithm( algorithm );
-        String result = instance.addUser();
-        org.junit.Assert.assertTrue( result.equalsIgnoreCase( "success" ) );
-
-        session.getTransaction().commit();
-
-        q = session.createQuery( queryString );
-        q.setString( "theName", this.getUsername() + "%" );
-        List userList2 = q.list();
-        session.close();
-        // user does exist
-        org.junit.Assert.assertEquals( 1, userList2.size() );
-
-        // try adding user again
-        try {
-            result = instance.addUser();
-            org.junit.Assert.assertTrue( result.equalsIgnoreCase( "duplicateUser" ) );
-        } catch ( Exception e ) {
-            System.out.println( "Intentional exception" );
-            System.out.println( e.getMessage() );
-        }
-      
-    } // end method testAddUser
-
-    /**
-     * Test of changeUser method, of class UserManagerBean.
-     */
-    @Test
-	public void testChangeUser() {
-        System.out.println( "changeUser" );
-        UserManagerBean instance = new UserManagerBean();
-        // instance.changeUser();
-        // TODO review the generated test code and remove the default call to fail.
-        // fail( "The test case is a prototype." );
-    } // end method testChangeUser
-
-    /**
-     * Test of dropUser method, of class UserManagerBean.
-     */
-    @Test
-	public void testDropUser() {
-        System.out.println( "dropUser" );
-        ActionEvent e = null;
-        UserManagerBean instance = new UserManagerBean();
-        String expResult = "";
-        // String result = instance.dropUser( e );
-        // assertEquals( expResult, result );
-        // TODO review the generated test code and remove the default call to fail.
-        // fail( "The test case is a prototype." );
-    } // end method testDropUser
-
-    /**
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * @return the hibernateUserName
-     */
-    public String getHibernateUserName() {
-        return hibernateUserName;
-    }
-
-    /**
-     * @param hibernateUserName the hibernateUserName to set
-     */
-    public void setHibernateUserName(String hibernateUserName) {
-        this.hibernateUserName = hibernateUserName;
+    public void demonstrateTestActorRef() {
+	ActorSystem system = ActorSystem.create("PiSystem");
+	final Props props = new Props(MyActor.class);
+	System.out.println( "props.dispatcher(): " + props.dispatcher() );
+	// orig:
+	final TestActorRef<MyActor> ref = TestActorRef.create(system, props, "testA");
+	
+	// final TestActorRef<MyActor> ref = TestActorRef.apply(props, system);
+	final MyActor actor = ref.underlyingActor();
+	assertTrue(actor.testMe());
+	// system.shutdown();
+	assertTrue( 1 == 1 );
     }
 
 
